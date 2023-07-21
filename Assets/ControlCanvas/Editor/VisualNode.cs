cc44b6126d2dd7b7c7e84708fb3f0b87e0e0c9d5
Felix-PC <fwavek@gmail.com>
2023-07-21 19:14:13 +0200

    Prepared node UXML and USS for custom adjustments

--- Assets/ControlCanvas/Editor/VisualNode.cs
+++ Assets/ControlCanvas/Editor/VisualNode.cs
@@ -1,6 +1,9 @@
+using System;
+using UnityEditor;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.UIElements;
+using System.Reflection;
 
 namespace ControlCanvas.Editor
 {
@@ -10,45 +13,78 @@ namespace ControlCanvas.Editor
 
         public Port portIn;
         public Port portOut;
-        
-        public VisualNode(Node node){
+
+        public VisualNode(Node node) : base("Assets/ControlCanvas/Editor/VisualNodeUXML.uxml")
+        {
+            //this.UseDefaultStyling();
+            //StyleSheet styleSheet = EditorGUIUtility.Load("StyleSheets/GraphView/Node.uss") as StyleSheet;
+            //StyleSheet styleSheet = EditorGUIUtility.Load("Assets/ControlCanvas/Editor/Node.uss") as StyleSheet;
+            //this.styleSheets.Add(styleSheet);  
+            
+            //WriteStyleSheetToFile(styleSheet, "Assets/ControlCanvas/Editor/Node.uss");
+            
             this.node = node;
             this.title = node.Name + node.Guid;
             this.viewDataKey = node.Guid;
             this.SetPosition(new Rect(node.Position, node.Size));
-            this.RegisterCallback((GeometryChangedEvent evt) => {
-                
+            this.RegisterCallback((GeometryChangedEvent evt) =>
+            {
+
                 node.Position = evt.newRect.position;
                 node.Size = evt.newRect.size;
             });
-            
+
             CreatePorts();
         }
 
         public void CreatePorts()
         {
-            portIn = InstantiatePort(Orientation.Horizontal, Direction.Output, Port.Capacity.Multi, typeof(bool));
+            portIn = InstantiatePort(Orientation.Horizontal, Direction.Input, Port.Capacity.Multi, typeof(bool));
             if (portIn != null)
             {
                 portIn.portName = "portIn";
                 inputContainer.Add(portIn);
             }
-            
-            portOut = InstantiatePort(Orientation.Horizontal, Direction.Input, Port.Capacity.Multi, typeof(bool));
+
+            portOut = InstantiatePort(Orientation.Horizontal, Direction.Output, Port.Capacity.Multi, typeof(bool));
             if (portOut != null)
             {
                 portOut.portName = "portOut";
                 outputContainer.Add(portOut);
             }
-            
-            
         }
         
-        // public override void SetPosition(Rect newPos)
-        // {
-        //     base.SetPosition(newPos);
-        //     node.Position = newPos.position;
-        // }
+        public static void WriteStyleSheetToFile(StyleSheet styleSheet, string path)
+        {
+            Assembly[] assemblies = AppDomain.CurrentDomain.GetAssemblies();
+            foreach (var assembly in assemblies)
+            {
+                Type type = assembly.GetType("Unity.UI.Builder.StyleSheetToUss");
+                if (type != null)
+                {
+                    // Get the method itself
+                    MethodInfo method = type.GetMethod("WriteStyleSheet", BindingFlags.Static | BindingFlags.Public);
+
+                    if (method == null)
+                    {
+                        Debug.LogError("Could not find method WriteStyleSheet");
+                        return;
+                    }
+
+                    // Create an instance of UssExportOptions
+                    Type optionsType = assembly.GetType("Unity.UI.Builder.UssExportOptions");
+                    object options = Activator.CreateInstance(optionsType);
+
+                    // Call the method
+                    method.Invoke(null, new object[] { styleSheet, path, options });
+                    return;
+                }
+            }
+        }
+
+
+
+        
         
     }
 }
