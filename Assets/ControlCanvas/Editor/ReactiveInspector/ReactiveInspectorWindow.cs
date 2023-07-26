293017f6e5e254e39ed25692cf9f21877b769ed9
Felix-PC <fwavek@gmail.com>
2023-07-26 15:36:50 +0200

    Readded 1d arrays for primitives and dataObjects

--- Assets/ControlCanvas/Editor/ReactiveInspector/ReactiveInspectorWindow.cs
+++ Assets/ControlCanvas/Editor/ReactiveInspector/ReactiveInspectorWindow.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Reflection;
-using Codice.CM.SEIDInfo;
-using UniRx;
-using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.UIElements;
@@ -65,8 +62,22 @@ namespace ControlCanvas.Editor.ReactiveInspector
                 element = uiField;
             }else if (t.IsArray)
             {
-                Debug.Log("Adding Array not implemented yet");
-                element = new Label("Array not implemented yet");
+                //Debug.Log("Adding Array not implemented yet");
+                //element = new Label("Array not implemented yet");
+                
+                Type elementType = t.GetElementType();
+                if (elementType == null)
+                {
+                    element = new Label($"Element Type is null");
+                }else if (elementType.IsArray)
+                {
+                    element = new Label($"Jagged Array not supported");
+                }
+                else
+                {
+                    element = AddArrayField(elementType, name);
+                }
+                
             }
             else if(t.IsClass)
             {
@@ -89,11 +100,36 @@ namespace ControlCanvas.Editor.ReactiveInspector
 
             return element;
         }
+
+        private static VisualElement AddArrayField(Type elementType, string s)
+        {
+            var foldout = new Foldout();
+            foldout.text = s;
+            foldout.name = s;
+            
+            var treeView = new TreeView();
+            treeView.virtualizationMethod = CollectionVirtualizationMethod.DynamicHeight;
+            //treeView.fixedItemHeight = 20;
+            // if (elementType.IsClass)
+            // {
+            //     treeView.fixedItemHeight = 80;
+            // }
+            
+            treeView.showAlternatingRowBackgrounds = AlternatingRowBackground.All;
+            treeView.name = s+"-TreeView";
+            treeView.makeItem = () => AddGenericField(elementType, s+"-Item");
+            
+            foldout.Add(treeView);
+            return foldout;
+        }
+
+
+        
         
         private static TField AddBaseField<T, TField>()
             where TField : BaseField<T>, new()
         {
-            var uiField = new TField { };
+            var uiField = new TField { label = "Not-Linked"};
             return uiField;
         }
 
@@ -111,14 +147,26 @@ namespace ControlCanvas.Editor.ReactiveInspector
                 handler(fieldName, fieldObject, uiField);
             }else if (t.IsArray)
             {
-                Debug.Log("Linking Array not implemented yet");
-                //TODO test this
-                // var array = (Array) fieldObject;
-                // for (int i = 0; i < array.Length; i++)
-                // {
-                //     var uiFieldChild = uiField.Q(fieldName);
-                //     LinkGenericField(array.GetValue(i), i.ToString(), uiFieldChild);
-                // }
+                //Debug.Log("Linking Array not implemented yet");
+
+                if (fieldObject == null)
+                {
+                    Debug.LogWarning($"FieldArray {fieldName} is null");
+                    return;
+                }
+                Type elementType = t.GetElementType();
+                if (elementType == null)
+                {
+                    Debug.LogWarning($"Linking Element Type is null");
+                }else if (elementType.IsArray)
+                {
+                    Debug.LogWarning($"Linking Jagged Array not supported");
+                }
+                else
+                {
+                    LinkArrayField(fieldObject, elementType, fieldName, uiField.Q<TreeView>());
+                }
+                
             }
             else if(t.IsClass)
             {
@@ -140,6 +188,33 @@ namespace ControlCanvas.Editor.ReactiveInspector
             }
         }
         
+        private static void LinkArrayField(object fieldObject, Type fieldObjectType, string fieldName,
+            VisualElement uiField)
+        {
+            Array array = (Array)fieldObject;
+            int id = 0;
+            int depth = 0;
+            int maxDepth = 10;
+            List<TreeViewItemData<object>> treeViewData = GetTreeViewItemData(array, depth, maxDepth, ref id);
+
+            if (uiField is TreeView treeView)
+            {
+                treeView.SetRootItems(treeViewData);
+                
+                treeView.bindItem = (VisualElement e, int index) =>
+                {
+                    object entry = treeView.GetItemDataForIndex<object>(index);
+                    
+                    LinkGenericField(entry, fieldObjectType, fieldName + index, e);
+                };
+                treeView.Rebuild();
+            }
+            else
+            {
+                Debug.LogError($"UIField {fieldName} is not a TreeView");
+            }
+        }
+        
         private static void LinkBaseField<T, TField>(string label, object fieldObject, VisualElement uiField)
             where TField : BaseField<T>
         {
@@ -158,6 +233,34 @@ namespace ControlCanvas.Editor.ReactiveInspector
                 Debug.LogError($"UIField is not of type {typeof(TField)}");
             }
         }
+        
+        private static List<TreeViewItemData<object>> GetTreeViewItemData(Array array, int id_depth, int id_maxDepth,
+            ref int id)
+        {
+            List<TreeViewItemData<object>> treeViewData = new List<TreeViewItemData<object>>();
+
+            if (id_depth > id_maxDepth)
+            {
+                //treeViewData.Add(new TreeViewItemData<object>(id_depth, "Max Depth Reached"));
+                //return treeViewData;
+                return null;
+            }
+
+            foreach (var item in array)
+            {
+                if (item is Array subArray)
+                {
+                    var subTree = GetTreeViewItemData(subArray, id_depth + 1, id_maxDepth, ref id);
+                    treeViewData.Add(new TreeViewItemData<object>(id++, item, subTree));
+                }
+                else
+                {
+                    treeViewData.Add(new TreeViewItemData<object>(id++, item));
+                }
+            }
+
+            return treeViewData;
+        }
     }
 
     public class DataContainer
@@ -166,10 +269,10 @@ namespace ControlCanvas.Editor.ReactiveInspector
         public string testString;
         public float testFloat;
         public bool testBool;
-        // public int[] testIntArray;
-        // public string[] testStringArray = new[] { "a", "b", "c" };
-        // public float[] testFloatArray;
-        // public bool[] testBoolArray = new[] { true, false, true, false, true, false };
+        public int[] testIntArray;
+        public string[] testStringArray = new[] { "a", "b", "c" };
+        public float[] testFloatArray;
+        public bool[] testBoolArray = new[] { true, false, true, false, true, false };
         // public int[,] testInt2DArray = new int[2, 2] { { 1, 2 }, { 3, 4 } };
         // public int[][] testIntJaggedArray = new int[2][] { new int[2] { 1, 2 }, new int[2] { 3, 4 } };
         // public string[][] testStringJaggedArray = new string[2][] { new string[2] { "a", "b" }, new string[2] { "c", "d" } };
@@ -179,17 +282,17 @@ namespace ControlCanvas.Editor.ReactiveInspector
             testInt2 = 2
         };
         
-        // public DataContainer2[] testContainer2Array = new DataContainer2[2]
-        // {
-        //     new DataContainer2()
-        //     {
-        //         testInt2 = 3
-        //     },
-        //     new DataContainer2()
-        //     {
-        //         testInt2 = 4
-        //     }
-        // };
+        public DataContainer2[] testContainer2Array = new DataContainer2[2]
+        {
+            new DataContainer2()
+            {
+                testInt2 = 3
+            },
+            new DataContainer2()
+            {
+                testInt2 = 4
+            }
+        };
     }
 
     public class DataContainer2
