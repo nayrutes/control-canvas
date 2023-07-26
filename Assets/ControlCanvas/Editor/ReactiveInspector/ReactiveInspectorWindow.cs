d637c44d93268d20d243a66c8965fba433301d60
Felix-PC <fwavek@gmail.com>
2023-07-26 12:26:23 +0200

    more generic inspector tests

--- Assets/ControlCanvas/Editor/ReactiveInspector/ReactiveInspectorWindow.cs
+++ Assets/ControlCanvas/Editor/ReactiveInspector/ReactiveInspectorWindow.cs
@@ -41,7 +41,7 @@ namespace ControlCanvas.Editor.ReactiveInspector
             // {typeof(string[]), typeof(TextField)},
         }; 
 
-        private static Dictionary<Type,Func<Type, VisualElement >> typeToCreateBaseFieldType = new ()
+        private static Dictionary<Type,Func<object, string, VisualElement >> typeToCreateBaseFieldType = new ()
         {
             {typeof(int), AddField<int,IntegerField>},
             {typeof(float), AddField<float,FloatField>},
@@ -70,7 +70,8 @@ namespace ControlCanvas.Editor.ReactiveInspector
             // Create a DataContainer and add it to the root
             DataContainer dataContainer = new DataContainer();
             root.Clear();
-            AddFieldsToRoot(dataContainer, root);
+            VisualElement holder = AddFieldsToRoot(dataContainer);
+            root.Add(holder);
         }
 
         private void OnDestroy()
@@ -82,25 +83,83 @@ namespace ControlCanvas.Editor.ReactiveInspector
             }
         }
 
-        private void AddFieldsToRoot(object obj, VisualElement root)
+        private VisualElement AddFieldsToRoot(object obj)
         {
-            // Get the type of the object
-            Type type = obj.GetType();
+            VisualElement holder = new VisualElement();
 
-            // Get all fields of the type
-            FieldInfo[] fields = type.GetFields(BindingFlags.Public | BindingFlags.Instance);
-
-            int depth = 0;
-            int maxDepth = 10;
-            
-            // Iterate over each field
-            foreach (var field in fields)
+            if (obj == null)
             {
-                object fieldObject = field.GetValue(obj);
-                Type t = field.FieldType;
-                Type t2 = fieldObject.GetType();
-                root.Add(ProcessArray(fieldObject));
+                //Debug.Log("Null object");
+                holder.Add(new Label($"Null object"));
+                return holder;
             }
+            
+            Type type = obj.GetType();
+            
+            if(type.IsArray)
+            {
+                //Debug.Log("Array type");
+                holder.Add(new Label($"Array of type {type}"));
+                
+                Type elementType = type.GetElementType();
+                if (elementType == null)
+                {
+                    holder.Add(new Label($"Element Type is null"));
+                }
+                else if (elementType.IsArray)
+                {
+                    //Process jaggedArrays
+                    // foreach (var element in array)
+                    // {
+                    //     arrayElement.Add(ProcessArray(element));  // Recurse into sub-arrays
+                    // }
+                    holder.Add(new Label($"Jagged Array not supported"));
+                }
+                else
+                {
+                    //Process non-jagged arrays
+                    return AddArrayField(obj);
+                }
+                
+                
+            }else if (type.IsPrimitive)
+            {
+                Debug.Log("Primitive type");
+            }else if (type.IsClass)
+            {
+                FieldInfo[] fields = type.GetFields(BindingFlags.Public | BindingFlags.Instance);
+                foreach (var field in fields)
+                {
+                    if (field.FieldType.IsPrimitive || field.FieldType == typeof(string))
+                    {
+                        holder.Add(ChooseField(field, obj));
+                    }else if (field.FieldType.IsClass)
+                    {
+                        var foldout = new Foldout();
+                        foldout.text = field.Name;
+                        foldout.Add(AddFieldsToRoot(field.GetValue(obj)));
+                        holder.Add(foldout);
+                    }
+                    else
+                    {
+                        Debug.Log($"Inner Type {field.FieldType} not supported");
+                    }
+                }
+            }
+            else
+            {
+                Debug.Log($"Type {type} not supported");
+            }
+            return holder;
+        }
+
+        private VisualElement ChooseField(FieldInfo field, object objectHolder)
+        {
+            if (typeToCreateBaseFieldType.TryGetValue(field.FieldType, out var baseFieldType))
+            {
+                return baseFieldType(field.GetValue(objectHolder), field.Name);
+            }
+            return new Label("No field type found");
         }
 
         public VisualElement ProcessArray(object obj)
@@ -146,11 +205,27 @@ namespace ControlCanvas.Editor.ReactiveInspector
             // }
         }
 
-        private static TField AddField<T, TField>(object fieldObject)
+        private static TField AddField<T, TField>(object fieldObject, string label)
             where TField : BaseField<T>, new()
         {
-            Type type = fieldObject.GetType();
-            var uiField = new TField { label = $"{type.Name}", value = (T)fieldObject };
+            //T value = (T)fieldInfo.GetValue(holderObject);
+            
+            //Type type = fieldObject.GetType();
+            //Type type = fieldInfo.Name.GetType();
+            var uiField = new TField { label = $"{label}", value = (T)fieldObject };
+            return uiField;
+        }
+        
+        private static TField AddField<T, TField>()
+            where TField : BaseField<T>, new()
+        {
+            var uiField = new TField {};
+            return uiField;
+        }
+        private static TField LinkField<T, TField>(string label, object fieldObject)
+            where TField : BaseField<T>, new()
+        {
+            var uiField = new TField { label = $"{label}", value = (T)fieldObject };
             return uiField;
         }
         
@@ -159,7 +234,7 @@ namespace ControlCanvas.Editor.ReactiveInspector
             Type type = fieldObject.GetType();
             if (typeToCreateBaseFieldType.TryGetValue(type, out var baseFieldType))
             {
-                VisualElement uiField = baseFieldType(type);
+                VisualElement uiField = baseFieldType(fieldObject, "?");
                 //var uiField = new TField { label = field.Name, value = value };
                 
                 //TODO readd Not using for testing now 
@@ -173,113 +248,6 @@ namespace ControlCanvas.Editor.ReactiveInspector
                 return uiField;
             }
             return new Label($"No handler for fieldObject with type {type}.");
-        }
-
-        
-        private VisualElement AddArrayField(object fieldObject)
-        {
-            Type type = fieldObject.GetType();
-            Type elementType = type.GetElementType();
-            Array array = (Array)fieldObject;
-
-            int id = 0;
-            int depth = 0;
-            int maxDepth = 10;
-            List<TreeViewItemData<object>> treeViewData = GetTreeViewItemData(array, depth, maxDepth, ref id);
-            
-            Foldout foldout = new Foldout
-            {
-                text = "Header"
-            };
-
-            var treeView = new TreeView();
-            foldout.Add(treeView);
-            treeView.fixedItemHeight = 20;
-            treeView.showAlternatingRowBackgrounds = AlternatingRowBackground.ContentOnly;
-            
-            treeView.SetRootItems(treeViewData);
-            treeView.makeItem = () =>
-            {
-                // typeToBaseFieldType.TryGetValue(elementType, out var t);
-                // return (BaseField<object>)t.Instantiate();
-                return AddField(null);
-            };
-            treeView.bindItem = (VisualElement element, int index) =>
-            {
-                if (element is BaseField<object> field)
-                {
-                    field.value = treeView.GetItemDataForIndex<object>(index);
-                    field.label = $"[{index}]";
-                }else
-                {
-                    Debug.LogError("Element is not a BaseField<object>");
-                }
-            };
-            treeView.Rebuild();
-            return foldout;
-
-        }
-        
-        
-        
-        
-        private int Depth(object obj, VisualElement root, FieldInfo field, int depth, int maxDepth)
-        {
-            if (field.FieldType.IsPrimitive || fieldHandlers.ContainsKey(field.FieldType)) //Allow custom types
-            {
-                // Check if we have a handler for this type
-                if (fieldHandlers.TryGetValue(field.FieldType, out var handler))
-                {
-                    // Call the handler to add the field to the root
-                    handler(field, obj, root, reactiveProperties);
-                }
-                else
-                {
-                    // If we don't have a handler, log a warning and add a label to the root
-                    Debug.LogWarning($"No handler for type {field.FieldType}.");
-                    root.Add(new Label($"No handler for field {field.Name} with type {field.FieldType}."));
-                }
-            }
-            else if (field.FieldType.IsArray)
-            {
-                
-                // Get the type of the elements of the array
-                Type elementType = field.FieldType.GetElementType();
-
-                //Detect jagged arrays and recurse
-                if (elementType != null && elementType.IsArray)
-                {
-                    var arrayFieldValue = field.GetValue(obj) as Array;
-                    if (arrayFieldValue != null)
-                    {
-                        foreach (var array in arrayFieldValue)
-                        {
-                            //depth = Depth(array, root, field., depth, maxDepth);
-                        }
-                    }
-                }
-                //Handle MultiDimensional Arrays and normal arrays
-                else// if (field.FieldType.GetArrayRank() > 1)
-                {
-                    AddArrayField(field, obj, root, reactiveProperties, fieldHandlers);
-                }
-                
-            }
-            else
-            {
-                if (depth < maxDepth)
-                {
-                    depth++;
-                    // If the field is not a primitive, recursively call AddFieldsToRoot
-                    AddFieldsToRoot(field.GetValue(obj), root);
-                }
-                else
-                {
-                    root.Add(new Label($"Max depth reached for field {field.Name} with type {field.FieldType}."));
-                }
-            }
-
-            return depth;
         }
         private static void AddField<T, TField>(FieldInfo field, object obj, VisualElement root,
             Dictionary<VisualElement, IDisposable> reactiveProperties)
@@ -300,6 +268,118 @@ namespace ControlCanvas.Editor.ReactiveInspector
             // Store the ReactiveProperty in the dictionary
             reactiveProperties[uiField] = reactiveProperty;
         }
+
+        
+        private VisualElement AddArrayField(object fieldObject)
+        {
+            Type type = fieldObject.GetType();
+            Type elementType = type.GetElementType();
+            Array array = (Array)fieldObject;
+
+            int id = 0;
+            int depth = 0;
+            int maxDepth = 10;
+            List<TreeViewItemData<object>> treeViewData = GetTreeViewItemData(array, depth, maxDepth, ref id);
+            
+            // Foldout foldout = new Foldout
+            // {
+            //     text = "Header"
+            // };
+
+            var treeView = new TreeView();
+            //foldout.Add(treeView);
+            treeView.fixedItemHeight = 20;
+            treeView.showAlternatingRowBackgrounds = AlternatingRowBackground.ContentOnly;
+            
+            treeView.SetRootItems(treeViewData);
+            treeView.makeItem = () =>
+            {
+                // typeToBaseFieldType.TryGetValue(elementType, out var t);
+                // return (BaseField<object>)t.Instantiate();
+                //return AddField(null);
+                return new Label("Test");
+            };
+            treeView.bindItem = (VisualElement element, int index) =>
+            {
+                if (element is BaseField<object> field)
+                {
+                    field.value = treeView.GetItemDataForIndex<object>(index);
+                    field.label = $"[{index}]";
+                }else if (element is Label label)
+                {
+                    label.text = $"[{index}]   {treeView.GetItemDataForIndex<object>(index)}";
+                }
+                else
+                {
+                    Debug.LogError("Element is not a BaseField<object>");
+                }
+            };
+            treeView.Rebuild();
+            return treeView;
+
+        }
+        
+        
+        
+        
+        // private int Depth(object obj, VisualElement root, FieldInfo field, int depth, int maxDepth)
+        // {
+        //     if (field.FieldType.IsPrimitive || fieldHandlers.ContainsKey(field.FieldType)) //Allow custom types
+        //     {
+        //         // Check if we have a handler for this type
+        //         if (fieldHandlers.TryGetValue(field.FieldType, out var handler))
+        //         {
+        //             // Call the handler to add the field to the root
+        //             handler(field, obj, root, reactiveProperties);
+        //         }
+        //         else
+        //         {
+        //             // If we don't have a handler, log a warning and add a label to the root
+        //             Debug.LogWarning($"No handler for type {field.FieldType}.");
+        //             root.Add(new Label($"No handler for field {field.Name} with type {field.FieldType}."));
+        //         }
+        //     }
+        //     else if (field.FieldType.IsArray)
+        //     {
+        //         
+        //         // Get the type of the elements of the array
+        //         Type elementType = field.FieldType.GetElementType();
+        //
+        //         //Detect jagged arrays and recurse
+        //         if (elementType != null && elementType.IsArray)
+        //         {
+        //             var arrayFieldValue = field.GetValue(obj) as Array;
+        //             if (arrayFieldValue != null)
+        //             {
+        //                 foreach (var array in arrayFieldValue)
+        //                 {
+        //                     //depth = Depth(array, root, field., depth, maxDepth);
+        //                 }
+        //             }
+        //         }
+        //         //Handle MultiDimensional Arrays and normal arrays
+        //         else// if (field.FieldType.GetArrayRank() > 1)
+        //         {
+        //             AddArrayField(field, obj, root, reactiveProperties, fieldHandlers);
+        //         }
+        //         
+        //     }
+        //     else
+        //     {
+        //         if (depth < maxDepth)
+        //         {
+        //             depth++;
+        //             // If the field is not a primitive, recursively call AddFieldsToRoot
+        //             AddFieldsToRoot(field.GetValue(obj), root);
+        //         }
+        //         else
+        //         {
+        //             root.Add(new Label($"Max depth reached for field {field.Name} with type {field.FieldType}."));
+        //         }
+        //     }
+        //
+        //     return depth;
+        // }
 
 
         private static void AddArrayField(FieldInfo field, object o, VisualElement root, Dictionary<VisualElement, IDisposable> disposables, Dictionary<Type, Action<FieldInfo, object, VisualElement, Dictionary<VisualElement, IDisposable>>> dictionary)
@@ -434,6 +514,27 @@ namespace ControlCanvas.Editor.ReactiveInspector
         public int[,] testInt2DArray = new int[2, 2] { { 1, 2 }, { 3, 4 } };
         public int[][] testIntJaggedArray = new int[2][] { new int[2] { 1, 2 }, new int[2] { 3, 4 } };
         public string[][] testStringJaggedArray = new string[2][] { new string[2] { "a", "b" }, new string[2] { "c", "d" } };
+
+        public DataContainer2 testContainer2 = new DataContainer2()
+        {
+            testInt2 = 2
+        };
+        
+        public DataContainer2[] testContainer2Array = new DataContainer2[2]
+        {
+            new DataContainer2()
+            {
+                testInt2 = 3
+            },
+            new DataContainer2()
+            {
+                testInt2 = 4
+            }
+        };
     }
-    
+
+    public class DataContainer2
+    {
+        public int testInt2;
+    }
 }
